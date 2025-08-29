ae01fcd4c15bf58821c8b750043830c247f46ee5
cliber castillo <cliberg@gmail.com>
2025-08-29 03:55:36 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+
+from fastapi import FastAPI
+import os
+
+app = FastAPI(title="MiBanco API", version="1.0.0")
+
+TEXT = os.getenv("TEXT", "hola mibanco")
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
+
+@app.get("/v1/mibanco")
+async def mibanco():
+    return {"message": TEXT}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
